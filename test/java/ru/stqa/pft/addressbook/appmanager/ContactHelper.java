89c6ce8ad827018038f294855d8f3b19a660e297
Evgeniia Ialymova <eugeniyayal@gmail.com>
2019-08-27 17:28:41 +0300

    final

---
+++ test/java/ru/stqa/pft/addressbook/appmanager/ContactHelper.java
@@ -0,0 +1,27 @@
+package ru.stqa.pft.addressbook.appmanager;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import ru.stqa.pft.addressbook.model.ContactData;
+
+public class ContactHelper extends HelperBase {
+
+    public ContactHelper(WebDriver wd) {
+        super(wd);
+    }
+
+    public void submitContactCreation() {
+        click(By.name("submit"));
+    }
+
+    public void fillContactForm(ContactData contactData) {
+        type(By.name("firstname"), contactData.getFirstnsme());
+        type(By.name("lastname"), contactData.getLastname());
+        type(By.name("mobile"), contactData.getMobile());
+        type(By.name("email"), contactData.getEmail());
+    }
+
+    public void initContactCreation() {
+        click(By.linkText("add new"));
+    }
+}
